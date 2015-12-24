6b782b0f4f86d9ce982382752d0810ce50a13095
Andrews Medina <andrewsmedina@gmail.com>
2015-12-24 13:53:21 -0200

    commands: add initial isntall command

---
+++ tsuru/commands.go
@@ -0,0 +1,27 @@
+// Copyright 2015 yati authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"github.com/tsuru/tsuru/cmd"
+	"launchpad.net/gnuflag"
+)
+
+type install struct {
+	fs *gnuflag.FlagSet
+}
+
+func (c *install) Info() *cmd.Info {
+	return &cmd.Info{
+		Name:    "install",
+		Usage:   "install",
+		Desc:    "",
+		MinArgs: 0,
+	}
+}
+
+func (c *install) Run(context *cmd.Context, client *cmd.Client) error {
+	return nil
+}
