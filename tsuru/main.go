76f5eb386cdea5c4c12ef12385e76d078efe635c
Andrews Medina <andrewsmedina@gmail.com>
2016-02-13 20:59:24 -0200

    move register commands to buiild manager

--- tsuru/main.go
+++ tsuru/main.go
@@ -14,13 +14,13 @@ const version = "0.1.0"
 
 func buildManager(name string) *cmd.Manager {
 	m := cmd.BuildBaseManager(name, version, "", nil)
+	m.Register(&install{})
+	m.Register(&uninstall{})
 	return m
 }
 
 func main() {
 	name := cmd.ExtractProgramName(os.Args[0])
 	manager := buildManager(name)
-	manager.Register(&install{})
-	manager.Register(&uninstall{})
 	manager.Run(os.Args[1:])
 }
