25e071e616022a3f5c07bfd8922e5e82a12fa4f0
Andrews Medina <andrewsmedina@gmail.com>
2016-02-13 16:18:49 -0200

    iaas/dm: get ip from the machine

--- tsuru/iaas/dockermachine/iaas.go
+++ tsuru/iaas/dockermachine/iaas.go
@@ -12,13 +12,27 @@ func init() {
 
 type dmIaas struct{}
 
-func (i *dmIaas) CreateMachine(params map[string]string) (*iaas.Machine, error) {
+func (i *dmIaas) createMachine() error {
 	cmd := exec.Command("docker-machine", "create", "tsuru", "-d", "virtualbox")
-	err := cmd.Run()
+	return cmd.Run()
+}
+
+func (i *dmIaas) getIP() (string, error) {
+	cmd := exec.Command("docker-machine", "ip", "tsuru")
+	output, err := cmd.Output()
+	return string(output), err
+}
+
+func (i *dmIaas) CreateMachine(params map[string]string) (*iaas.Machine, error) {
+	err := i.createMachine()
 	if err != nil {
 		return nil, err
 	}
-	return &iaas.Machine{}, nil
+	ip, err := i.getIP()
+	if err != nil {
+		return nil, err
+	}
+	return &iaas.Machine{Address: ip}, nil
 }
 
 func (i *dmIaas) DeleteMachine(m *iaas.Machine) error {
